34824b33316e5ca3c40ed9c307b339b66566808f
tushar-oasis <tushar.kokane15@gmail.com>
2016-10-16 23:17:55 +0530

    First commit

---
+++ app/src/main/java/com/oasis/interactivestory/model/Choice.java
@@ -0,0 +1,30 @@
+package com.oasis.interactivestory.model;
+
+/**
+ * Created by Oasis on 29-04-2016.
+ */
+public class Choice {
+    private String mText;
+    private int mNextPage;
+
+    public Choice(String text, int nextPage) {
+        mText = text;
+        mNextPage = nextPage;
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+
+    public int getNextPage() {
+        return mNextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        mNextPage = nextPage;
+    }
+}
